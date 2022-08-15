d6b76b8e8a9bc84971c75423821f5e8a91c2152d
ragu-cell <ragusivraj@gmail.com>
2022-08-15 07:43:47 -1000

    third

---
+++ src/org/com/Git.java
@@ -0,0 +1,13 @@
+package org.com;
+
+public class Git {
+	
+
+	private static int regnio;
+	private static String name;
+	public void CompreToTwoObject(int regnio,String name) {
+		this.name=name;
+		this.regnio=regnio;
+
+}
+}
